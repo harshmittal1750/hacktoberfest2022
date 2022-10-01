3e4009c3a6ba5fd0c40d030bfe600cbafe6409c5
zijing.niuace <zijing.niuace@gmail.com>
2022-10-01 23:35:12 +0800

    Quick Sort

---
+++ Python/quickSort.py
@@ -0,0 +1,34 @@
+def partition(array, low, high):
+
+    pivot = array[high]
+
+    i = low - 1
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+
+    return i + 1
+
+def quickSort(array, low, high):
+    if low < high:
+
+        pi = partition(array, low, high)
+
+        quickSort(array, low, pi - 1)
+        quickSort(array, pi + 1, high)
+
+
+arr = [1012, 223217, 14121, -12312, -21110, 119, -22]
+print("Unsorted Array")
+print(arr)
+
+size = len(arr)
+
+quickSort(arr, 0, size - 1)
+
+print('Sorted Array in Ascending Order:')
+print(arr)
